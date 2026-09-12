4dfd49571b8b6335e3f8de34140927ea6767796d
carinimaurizio2-code <carini.maurizio2@gmail.com>
2026-09-12 08:41:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,17 @@
-const CACHE = 'quaterna-v1';
-const ASSETS = [
-  './',
-  './quaterna_autore.html',
-  './manifest.json',
-  './storico.txt',
-  './estrazioni.txt',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
+const CACHE = 'quaterna-v5';
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
+    fetch(e.request).then(resp => {
       const copy = resp.clone();
       caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
       return resp;
-    }).catch(() => cached))
+    }).catch(() => caches.match(e.request))
   );
 });
